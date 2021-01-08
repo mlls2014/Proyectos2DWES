5d01f80f0e7c1aa0f1f1aaebadf07dfd46bd8bd0
mlls2014 <manlucsan@gmail.com>
2021-01-08 03:22:22 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Lista proyectos disponibles</title>
+	<style type="text/css">
+	<!--
+
+   body {
+	   margin: 0;  padding: 0;  margin-bottom: 15px;  margin-top: 8px;
+	   background: #DDD;
+   }
+
+</style>
+</head>
+<body>
+	<h1>Proyectos de ejemplo para clase</h1>
+   <ol>
+      <li><a href="ejem-proy">Primer proyecto Laravel 8</a></li>                
+   </ol>
+</body>
+</html>
+
+
